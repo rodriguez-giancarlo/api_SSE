48bf0ce7e17c50a6136e76fe9eebf8b5824a4770
rodriguez-giancarlo <92653419+rodriguez-giancarlo@users.noreply.github.com>
2021-11-03 20:03:31 -0500

    controlador de historial

--- src/controllers/historialController.js
+++ src/controllers/historialController.js
@@ -11,5 +11,30 @@ controller.guardar=(req,res)=>{
         })
     })
 }
+controller.listar=(req,res)=>{
+    req.getConnection((err,conn)=>{
+        if(err) return res.send(err)
+
+        conn.query('SELECT H.idHistorial,H.fecha,H.tiempo,H.idpersona,R.nombre,P.nombre,P.apellido,P.urlFoto, COUNT(*) as visto FROM recurso R INNER JOIN historial H ON R.idRecurso=H.idRecurso INNER JOIN persona P ON H.idpersona=P.idpersona WHERE H.idRecurso=? GROUP BY H.idpersona;',[req.params.idRecurso],(err,rows)=>{
+            if(err){
+                return res.json(err)
+            } 
+            res.json(rows)
+        })
+    })
+};
+
+controller.listarAlumnosMatriculadosXCurso=(req,res)=>{
+    req.getConnection((err,conn)=>{
+        if(err) return res.send(err)
+
+        conn.query('SELECT C.idCurso,C.nombre,COUNT(*) as matriculados FROM curso C INNER JOIN matricula M ON C.idCurso = M.idCurso WHERE C.idpersona=? GROUP BY C.idCurso UNION SELECT C.idCurso,C.nombre,COALESCE(M.idUsuCurso, 0) as matriculados FROM curso C LEFT JOIN matricula M ON C.idCurso = M.idCurso WHERE M.idUsuCurso IS NULL AND C.idpersona=?;',[req.params.idpersona,req.params.idpersona],(err,rows)=>{
+            if(err){
+                return res.json(err)
+            } 
+            res.json(rows)
+        })
+    })
+};
 
 module.exports=controller;
--- src/routes/historial.js
+++ src/routes/historial.js
@@ -3,5 +3,8 @@ const router=Router();
 const historialController= require('../controllers/historialController');
 
 router.post('/save',historialController.guardar);
+router.get('/:idRecurso',historialController.listar);
+router.get('/matriculadosXcurso/:idpersona',historialController.listarAlumnosMatriculadosXCurso);
+
 
 module.exports=router;
