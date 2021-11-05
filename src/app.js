8534d1ba65270022dabb7cd955860858a01ca4af
rodriguez-giancarlo <92653419+rodriguez-giancarlo@users.noreply.github.com>
2021-11-05 14:05:55 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -19,7 +19,7 @@ const historialRoutes=require('./routes/historial');
 // configuraciones
 app.set('port',process.env.PORT || 4000);
 app.set('view engine','ejs');
-app.set('views',path.join(__dirname,'views'));
+// app.set('views',path.join(__dirname,'views'));
 
 
 // middlewares
